1669ce7b9c02e8ef7d3a9f5f358c23766d9d48c6
anna <anna@rcbd.org>
2023-05-17 21:21:19 -0400

    styling

--- App.tsx
+++ App.tsx
@@ -8,19 +8,64 @@ function App(): JSX.Element {
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add Goal" />
+        <Button  title="Add Goal" />
       </View>
       <View>
         <Text>List of goals...</Text>
       </View>
+      {/* <View
+        style={{
+          padding: 50,
+          flexDirection: 'row',
+          width: '100%',
+          height: 300,
+          backgroundColor: 'yellow',
+          justifyContent: 'space-around',
+          alignItems: 'stretch',
+        }}>
+        <View
+          style={{
+            backgroundColor: 'red',
+            flex: 1,
+            height: 100,
+            width: 100,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>1</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: 'green',
+            flex: 1,
+            height: 100,
+            width: 100,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>2</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: 'blue',
+            flex: 1,
+            height: 100,
+            width: 100,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>3</Text>
+        </View>
+      </View> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    flex: 1,
-    padding: 50,
+    // flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
     flexDirection: 'row',
@@ -29,7 +74,7 @@ const styles = StyleSheet.create({
   textInput: {
     borderWidth: 1,
     borderColor: '#ccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8,
   },
