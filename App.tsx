db2d7ad446959c36de76f0a4a88e8fe4f8d75880
anna <anna@rcbd.org>
2023-05-17 22:10:53 -0400

    Managing A List of Course

--- App.tsx
+++ App.tsx
@@ -30,52 +30,11 @@ function App(): JSX.Element {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {/* <Text>List of goals...</Text> */}
+        {courseGoals.map(goal => (
+          <Text key={goal}>{goal}</Text>
+        ))}
       </View>
-      {/* <View
-        style={{
-          padding: 50,
-          flexDirection: 'row',
-          width: '100%',
-          height: 300,
-          backgroundColor: 'yellow',
-          justifyContent: 'space-around',
-          alignItems: 'stretch',
-        }}>
-        <View
-          style={{
-            backgroundColor: 'red',
-            flex: 1,
-            height: 100,
-            width: 100,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Text>1</Text>
-        </View>
-        <View
-          style={{
-            backgroundColor: 'green',
-            flex: 1,
-            height: 100,
-            width: 100,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Text>2</Text>
-        </View>
-        <View
-          style={{
-            backgroundColor: 'blue',
-            flex: 1,
-            height: 100,
-            width: 100,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Text>3</Text>
-        </View>
-      </View> */}
     </View>
   );
 }
---
+++ README.md
@@ -0,0 +1,47 @@
+{/* <View
+        style={{
+          padding: 50,
+          flexDirection: 'row',
+          width: '100%',
+          height: 300,
+          backgroundColor: 'yellow',
+          justifyContent: 'space-around',
+          alignItems: 'stretch',
+        }}>
+        <View
+          style={{
+            backgroundColor: 'red',
+            flex: 1,
+            height: 100,
+            width: 100,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>1</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: 'green',
+            flex: 1,
+            height: 100,
+            width: 100,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>2</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: 'blue',
+            flex: 1,
+            height: 100,
+            width: 100,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>3</Text>
+        </View>
+      </View> */}
+    </View>
+  );
+}
